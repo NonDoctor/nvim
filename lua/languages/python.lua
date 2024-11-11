b33480d99970a9e6dd8a498cc136f1593ad233fe
NonDoctor <tsucintosh@gmail.com>
2024-11-11 12:05:15 +0300

    update

--- lua/languages/python.lua
+++ lua/languages/python.lua
@@ -1,25 +1,37 @@
 return {
-     {
+    -- Установка и настройка mason-lspconfig для управления LSP серверами
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = { "williamboman/mason.nvim" },
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "pyright" }, -- Список LSP-серверов для автоматической установки
+                automatic_installation = true,
+            })
+        end,
+    },
+
+    -- Конфигурация LSP через nvim-lspconfig
+    {
         "neovim/nvim-lspconfig",
         config = function()
             local lspconfig = require("lspconfig")
-
-            -- Настройка для Python (pyright)
+            -- Настройка LSP сервера для Python (pyright)
             lspconfig.pyright.setup({})
         end,
     },
 
-    -- mason-null-ls для установки дополнительных утилит, таких как goimports и golines
+    -- Настройка mason-null-ls для установки дополнительных инструментов форматирования и линтинга
     {
         "jayp0521/mason-null-ls.nvim",
         dependencies = { "williamboman/mason.nvim", "jose-elias-alvarez/null-ls.nvim" },
         config = function()
             require("mason-null-ls").setup({
-                ensure_installed = { "black" }, -- Автоматическая установка утилит
+                ensure_installed = { "black" }, -- Утилиты для автоматической установки
                 automatic_installation = true,
             })
 
-            -- Настройка null-ls для форматирования и линтинга
+            -- Конфигурация null-ls для Python форматирования и линтинга
             local null_ls = require("null-ls")
             null_ls.setup({
                 sources = {
@@ -29,3 +41,4 @@ return {
         end,
     },
 }
+
